27318f1d5b1cd95e883b78c3ec00617a40199cd6
niko <nickosarmijo4@gmail.com>
2026-08-20 12:14:47 +0800

    Add public Projects API endpoint

---
+++ backend/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::query()
+            ->where('is_published', true)
+            ->orderBy('display_order')
+            ->get();
+
+        return response()->json($projects);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
